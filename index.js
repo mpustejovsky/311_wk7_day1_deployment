fec72ea97ae55a18a4a1329e8bb14a26de18ab09
mpustejovsky <mpustejovsky@yahoo.com>
2020-10-14 12:52:09 -0500

    week 8 adder

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use('/users', usersRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our server!')
+  res.send('Welcome to our new server!')
 })
 
 app.listen(port, () => {
- console.log(`Web server is listening on port ${port}!`);
+  console.log(`Web server is listening on port ${port}!`);
 });
